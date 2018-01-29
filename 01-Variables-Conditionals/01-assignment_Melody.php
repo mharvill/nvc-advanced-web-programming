5c55f1d1923e4d9041259556970a6c1bca14bc89
Melody <melody.harvill9@gmail.com>
2018-01-28 20:21:37 -0600

    Added number comments

---
+++ 01-Variables-Conditionals/01-assignment_Melody.php
@@ -0,0 +1,48 @@
+<?php
+
+// 01. Create and use a string variable
+
+$title = "Advanced Web Programming";
+
+// 02. Create and use an integer variable
+
+$number = 8;
+
+// 03. Create and use a boolean variable
+
+$
+
+// 04. Create and call one or more function(s)
+
+
+
+$pets = [
+
+"Dogs",
+"a snake",
+"Budgies"
+
+];
+
+$selectedpets = $pets[(1)];
+
+?>
+
+<html>
+<head>
+
+<title> <?php echo $title; ?> </title> 
+
+</head
+<body>
+
+<h1> <?php echo $title; ?> </h1>
+
+<h4><?php echo "<p> Hello World </p>"; ?></h4>
+
+<?php echo "My favorite pet I own is " .$selectedpets?>;
+
+<?php echo "How many pets do I have?: " .$number?>
+
+</body>
+</html>
