42730230adff52d2d99486fcaae556d2f9443d94
Melody <melody.harvill9@gmail.com>
2018-02-04 00:43:25 -0600

    Numbers 1-4

--- 02-Loops-Arrays-Functions/02-assignment.php
+++ 02-Loops-Arrays-Functions/02-assignment.php
@@ -7,30 +7,53 @@
         to this assignment below.
 *****************************************/
 
-
-
 // 01. Create and Call at least one function.
 
+function myName(){
 
+echo "My name is Melody Ann Harvill. <br>";
+}
 
+myName();
 
 
 // 02. Create a For Loop
 
+echo "<p> I have </p>";
+
+for ($count = 0; $count <= 4; $count++) {
 
 
+echo "<p> $count </p>";
+
+}
+
+echo "siblings.<br>";
 
 
 // 03. Create a While Loop
 
+$number = 1;
 
+while($number <= 13) {
 
+ echo "<br>Count to the number: $number <br>";
+
+$number++;
+
+}
 
 
 // 04. Create an Array
 
+$siblings["Michael", "Michaela", "Deana", "McCaleb"];
 
+echo "My favorite siblings are" .$siblings[1]. "and" .$siblings[2]. ".";
 
+<br>
+<br>
+
+echo "Just kidding...";
 
 
 // 05. Sort an Array
